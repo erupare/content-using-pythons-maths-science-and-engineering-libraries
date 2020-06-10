ec446deb390684395336c3d907dba546b80fb5bb
Keith Thompson <me@keiththomps.com>
2020-06-10 16:43:16 -0400

    Update setup.sh to perform entire installation.

--- jupyter-setup/setup.sh
+++ jupyter-setup/setup.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-# copy jupyter_notebook_config.py > /home/cloud_user/.jupyter/jupyter_notebook_config.py
-# copy jupyter.service > /etc/systemd/system/jupyter.service
-
-pyenv shell 3.8.2
+# Expects python version to be 3.8.2 from pyenv
 
 python -m pip install jupyter
 
-sudo systemctl start jupyter.service
+mkdir -p ~/{.jupyter,notebooks}
+
+curl -o ~/.jupyter/jupyter_notebook_config.py https://raw.githubusercontent.com/linuxacademy/content-using-pythons-maths-science-and-engineering-libraries/master/jupyter-setup/jupyter_notebook_config.py
+
+sudo curl -o /etc/systemd/system/jupyter.service https://raw.githubusercontent.com/linuxacademy/content-using-pythons-maths-science-and-engineering-libraries/master/jupyter-setup/jupyter.service
+
+sudo systemctl enable --now jupyter.service
